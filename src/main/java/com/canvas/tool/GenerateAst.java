6134fdcb5710e2f87d51a4ff61d5759e97906808
Canvas02 <Canvas02@protonmail.com>
2023-04-19 19:11:56 +0300

    Added visitor to Expr inheritor

--- src/main/java/com/canvas/tool/GenerateAst.java
+++ src/main/java/com/canvas/tool/GenerateAst.java
@@ -27,13 +27,14 @@ public class GenerateAst {
 
     /**
      * A function that generates AST classes for the lox interpreter and writes to a file
+     *
      * @param outputDir The directory to write a file in
      * @param baseName  The name of the abstract class that encapsulates the other generates classes,
      *                  it is also used as the name for the generates file
      * @param types     The types to generate as in the following format: <br/>
      *                  <code>ClassName   : Type1 param1, Type2 param2, ...etc</code>
-     * @throws IOException  If the output directory (outputDir argument) does not exist,
-     *                      the function will fail to create a file
+     * @throws IOException If the output directory (outputDir argument) does not exist,
+     *                     the function will fail to create a file
      */
     private static void defineAst(
             String outputDir,
@@ -48,12 +49,19 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
+        // The AST classes
         for (String type : types) {
             var splitString = type.split(":");
             String className = splitString[0].trim();
             String fields = splitString[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        // The base 'accept' method
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
 
@@ -62,6 +70,25 @@ public class GenerateAst {
 
     /**
      * Helper function for defineAst
+     *
+     * @param writer    the file writer
+     * @param baseName the name of the encapsulating class
+     * @param types The types to generate visitor functions for
+     */
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.printf("     R visit%s%s(%s %s);\n", typeName, baseName, typeName, baseName.toLowerCase());
+        }
+
+        writer.println("    }");
+    }
+
+    /**
+     * Helper function for defineAst
+     *
      * @param writer    the file writer
      * @param baseClass the name of the encapsulating class
      * @param className the name of the encapsulated class
@@ -85,13 +112,20 @@ public class GenerateAst {
         // Constructor
         writer.printf("         %s(%s){\n", className, fieldList);
 
-        for (String field : fields ) {
+        for (String field : fields) {
             String name = field.split(" ")[1];
             writer.printf("            this.%s = %s;\n", name, name);
         }
 
         writer.printf("     }\n");
         // Constructor - end
+
+        // Visitor pattern
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.printf("        return visitor.visit%s%s(this);\n", className, baseClass);
+        writer.println("    }");
 
         writer.printf("    }\n");   // Class end
     }
